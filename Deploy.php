99ee5e2569cd813a825fa334100aff7220450e0f
vovantune <tune@artskills.ru>
2021-03-15 22:41:04 +0300

    Initial commit

---
+++ Deploy.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+class Deploy
+{
+    /**
+     * Deploy constructor.
+     *
+     * @param string $deployConfig Папка конфигурации
+     * @param string $deployDir Папка, в которой развёрнуто тестовое окружение
+     */
+    public function __construct(string $deployConfig, string $deployDir)
+    {
+
+    }
+
+    /**
+     * Стартовая инициализация
+     */
+    public function setUp()
+    {
+
+    }
+
+    /**
+     * Необходимые операции при окончании
+     */
+    public function tearDown()
+    {
+
+    }
+}
